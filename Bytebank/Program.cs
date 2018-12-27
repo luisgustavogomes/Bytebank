38889cc8078d2589d22e956da3e9cb3cb90c7f62
luisgustavogomes <luisgustavogomes@outlook.com>
2018-12-27 08:17:31 -0200

    Alterações

--- Bytebank/Program.cs
+++ Bytebank/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bytebank.Funcionarios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,36 @@ namespace Bytebank
     {
         static void Main(string[] args)
         {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario carlos = new Funcionario
+            {
+                Nome = "Carlos",
+                Cpf = "999.999.999-99",
+                Salario = 2000
+            };
+
+            gerenciador.Registrar(carlos);
+
+            Diretor roberta = new Diretor
+            {
+                Nome = "Roberta",
+                Cpf = "999.999.999-99",
+                Salario = 5000
+            };
+
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine("Nome: " + carlos.Nome);
+            Console.WriteLine("Bonificação: " + carlos.GetBonificacao());
+
+            Console.WriteLine("Nome: " + roberta.Nome);
+            Console.WriteLine("Bonificação: " + roberta.GetBonificacao());
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Total bonificação: " + gerenciador.GetTotalBonificacao());
+
+            Console.ReadLine();
         }
     }
 }
